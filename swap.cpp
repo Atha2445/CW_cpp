f5b221846bd5f0cca82f3afe16bb88b87405bc70
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-09-30 15:34:58 +0530

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,83 @@
+// C++ program to swap two
+// numbers using swap()
+// function
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver code
+int main()
+{
+	int a = 5, b = 10;
+
+	cout << "Before swapping a = " << a << " , b = " << b
+		<< endl;
+
+	// Built-in swap function
+	swap(a, b);
+
+	cout << "After swapping a = " << a << " , b = " << b
+		<< endl;
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+// C++ program to swap two
+// numbers without using 3rd
+// variable
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver code
+int main()
+{
+	int a = 2, b = 3;
+
+	cout << "Before swapping a = " << a << " , b = " << b
+		<< endl;
+
+	// applying algorithm
+	b = a + b;
+	a = b - a;
+	b = b - a;
+
+	cout << "After swapping a = " << a << " , b = " << b
+		<< endl;
+	return 0;
+}
+
+
+
+
+
+// C++ program to swap two
+// numbers using 3rd variable
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver code
+int main()
+{
+	int a = 2, b = 3;
+
+	cout << "Before swapping a = " << a << " , b = " << b
+		<< endl;
+
+	// temporary variable
+	int temp;
+
+	// appying swapping algorithm
+	temp = a;
+	a = b;
+	b = temp;
+	cout << "After swapping a = " << a << " , b = " << b
+		<< endl;
+
+	return 0;
+}
