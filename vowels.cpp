fb5adc7b43561a019da421c870a8449792087ee9
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-10-01 16:57:06 +0530

    Create vowels.cpp

---
+++ vowels.cpp
@@ -0,0 +1,40 @@
+//Code for vowels and consonant using switch cases 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  char ch;
+  cin>>ch;
+
+  switch(ch){
+
+    case 'a':
+    cout<<"Vowels"<<endl;
+    break;
+
+    case 'e':
+    cout<<"Vowels"<<endl;
+    break;
+
+    case 'i':
+    cout<<"Vowels"<<endl;
+    break;
+
+    case 'o':
+    cout<<"Vowels"<<endl;
+    break;
+
+
+    case 'u':
+    cout<<"Vowels"<<endl;
+    break;
+
+    default:
+    cout<<"Consonants"<<endl;
+    break;
+
+  }
+  return 0;
+  
+
+}
