a52f3f2c1f3b77efd3d8ace77505eae37ae5d6dd
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-10-08 17:02:10 +0530

    Create pattern_Q1.cpp

---
+++ pattern_Q1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i,j,m,n;
+    cin>>m>>n;
+    for(i=1;i<m;i++){
+        for(j=1;j<n;j++){
+             std::cout << "*" ;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
