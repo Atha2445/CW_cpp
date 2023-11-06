780cd6b122b2f45c3d5d9e9a34c8179e5fa1e650
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-11-06 17:41:31 +0530

    Create runningSum.cpp

---
+++ runningSum.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void runningSum(vector<int> &v){
+    for(int i=0;i<v.size();i++){
+        v[i]+=v[i-1];
+    }
+    
+    return;
+} 
+
+int main(){
+    int n;
+    cin>>n;
+    
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        int ele;
+        cin>>ele;
+        v.push_back(ele);   
+    }
+    runningSum(v);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    
+    return 0;
+}
