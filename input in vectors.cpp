877de9f5d0991c648de5855780971790f62e5d18
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-12-20 16:00:04 +0530

    Create input in vectors.cpp

---
+++ input in vectors.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main() {
+    int n;
+    cin>>n;
+    
+	vector<int> a;
+	for(int i=0;i<n;i++){
+	    int ele;
+	    cin>>ele;
+	    a.push_back(ele);
+	}
+	for(int num:a){
+	    cout<<num<<" ";
+	}
+	cout<<endl;
+	
+	return 0;
+}
