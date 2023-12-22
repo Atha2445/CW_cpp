a5d9044edaf179ab96ea751cdc1520d93e37d8b2
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-12-22 10:41:33 +0530

    Create ascending order using array.cpp

---
+++ ascending order using array.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,min;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i+ 1;j<n;j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+        int t=arr[i];
+        arr[i]=arr[min];
+        arr[min]=t;
+        for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+    }
+}
